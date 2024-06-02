d003329cf3984ade3b9cad4ecd95be9249bcaab9
Subhradip-Das123 <124434372+Subhradip-Das123@users.noreply.github.com>
2024-06-02 13:59:08 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Navbar from './component/Navbar';
+import TextForm from './component/Textform';
+import React,{useState} from 'react';
+import Alert from './component/Alert'
 import './App.css';
 
+// import About from './component/About';
+// let name = "Subhra"
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor='grey'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+    }
+  }
   return (
-   <div className="blank">Lovely</div>
+    <>
+      {/* <Navbar title="Textutils2" /> */}
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert/>
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze"/>
+        {/* <About /> */}
+      </div>
+    </>
   );
 }
 
